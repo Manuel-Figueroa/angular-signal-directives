a9c18f724769cd82417294c2e98a82a0e2190ef2
Manuel-Figueroa <figueroagarciamanuel@gmail.com>
2023-06-16 20:48:34 -0600

    signals

---
+++ src/app/signals/layout/signals-layout/signals-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './signals-layout.component.html',
+  styleUrls: ['./signals-layout.component.css']
+})
+export class SignalsLayoutComponent {
+  public onFielUpdated(field:string,value:string){
+
+  }
+}
